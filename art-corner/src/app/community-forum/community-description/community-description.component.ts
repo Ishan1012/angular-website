36dcc3b473dde61ccc717fe141184468f0aacc3c
Ishan Dwivedi <ishandwivedi1210@gmail.com>
2024-11-16 08:31:54 +0530

    Minor updates 3.6.4

--- art-corner/src/app/community-forum/community-description/community-description.component.ts
+++ art-corner/src/app/community-forum/community-description/community-description.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CreateCommunities } from '../../shared/model/CreateCommunities';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-community-description',
@@ -11,11 +12,23 @@ export class CommunityDescriptionComponent {
   user!: CreateCommunities;
 
   constructor(
-    private activeRoute: ActivatedRoute
+    private activeRoute: ActivatedRoute,
+    @Inject(PLATFORM_ID) private platformId: any,
   ) {
     const item = this.activeRoute.snapshot.paramMap.get('commid');
     this.user = JSON.parse(item || '') as CreateCommunities;
   }
 
-  
+  ngOnInit() {
+    this.scrollToTop();
+  }
+
+  scrollToTop() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
