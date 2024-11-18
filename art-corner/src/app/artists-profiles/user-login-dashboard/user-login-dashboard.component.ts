26afaeb93abcc2911e9888c27ed8d3e2a3260ccc
Ishan Dwivedi <ishandwivedi1210@gmail.com>
2024-11-18 12:23:50 +0530

    Minor updates 3.7.14

--- art-corner/src/app/artists-profiles/user-login-dashboard/user-login-dashboard.component.ts
+++ art-corner/src/app/artists-profiles/user-login-dashboard/user-login-dashboard.component.ts
@@ -3,6 +3,8 @@ import { User } from '../../shared/model/User';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login-dashboard',
@@ -10,12 +12,16 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrl: './user-login-dashboard.component.css'
 })
 export class UserLoginDashboardComponent {
+  selectedFile: File | null = null;
+  postForm!: FormGroup;
   user!: User;
 
   constructor(
     private userServices: UserService,
     private router: Router,
-    @Inject(PLATFORM_ID) private platformId: any
+    private fromBuilder: FormBuilder,
+    @Inject(PLATFORM_ID) private platformId: any,
+    private http: HttpClient
   ) {
     this.userServices.userObservable.subscribe((user) => {
       this.user = user;
@@ -26,6 +32,9 @@ export class UserLoginDashboardComponent {
 
   ngOnInit() {
     this.scrollToTop();
+    this.postForm = this.fromBuilder.group({
+      name: [''],
+    });
   }
 
   scrollToTop() {
@@ -39,5 +48,29 @@ export class UserLoginDashboardComponent {
 
   logout(){
     this.userServices.logout();
+  }
+
+  onFileSelected(event: Event): void {
+    // const input = event.target as HTMLInputElement;
+    // if (input.files && input.files.length > 0) {
+    //   this.selectedFile = input.files[0];
+    // }
+  }
+
+  // Submit the selected file
+  onSubmit(): void {
+    // if (!this.selectedFile) return;
+
+    // const formData = new FormData();
+    // formData.append('image', this.selectedFile);
+
+    // this.http.post('http://your-backend-api/upload', formData).subscribe({
+    //   next: (response) => {
+    //     console.log('Upload successful', response);
+    //   },
+    //   error: (error) => {
+    //     console.error('Upload failed', error);
+    //   }
+    // });
   }
 }
