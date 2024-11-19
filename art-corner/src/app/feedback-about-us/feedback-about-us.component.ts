2e6664ae07451d40500f1e97d3502901db5a5623
Ishan Dwivedi <ishandwivedi1210@gmail.com>
2024-11-19 11:45:54 +0530

    Feedback Form update 3.8.7

--- art-corner/src/app/feedback-about-us/feedback-about-us.component.ts
+++ art-corner/src/app/feedback-about-us/feedback-about-us.component.ts
@@ -1,5 +1,8 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-feedback-about-us',
@@ -7,12 +10,26 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
   styleUrl: './feedback-about-us.component.css'
 })
 export class FeedbackAboutUsComponent {
+  postForm!: FormGroup;
+  returnUrl = '';
 
-  constructor(@Inject(PLATFORM_ID) private platformId: any)
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: any,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private fromBuilder: FormBuilder,
+  )
   {}
 
   ngOnInit() {
-    this.scrollToTop();
+    this.scrollToTop();this.postForm = this.fromBuilder.group({
+      name: ['',Validators.required],
+      email: ['',[Validators.required, Validators.email]],
+      subject: ['',Validators.required],
+      message: ['',Validators.required]
+    });
+    
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
   }
 
   scrollToTop() {
@@ -34,5 +51,36 @@ export class FeedbackAboutUsComponent {
       console.error(`Element with ID "${elementId}" 
    not found.`);
     }
+  }
+
+  onSubmit(): void{
+    if(this.postForm.invalid) {
+      const nameControl = this.postForm.get('name');
+      const emailControl = this.postForm.get('email');
+      const subjectControl = this.postForm.get('subject');
+      const messageControl = this.postForm.get('message');
+
+      if(nameControl?.hasError("required")) {
+        this.toastr.error('Name is required!', 'Feedback Submission Failed!');
+      }
+
+      if(emailControl?.hasError("required")) {
+        this.toastr.error('Email is required!', 'Feedback Submission Failed!');
+      }
+      else if(emailControl?.hasError("email")) {
+        this.toastr.error('Email is invalid!', 'Feedback Submission Failed!');
+      }
+
+      if(subjectControl?.hasError("required")) {
+        this.toastr.error('Subject is required!', 'Feedback Submission Failed!');
+      }
+      
+      if(messageControl?.hasError("required")) {
+        this.toastr.error('Message is required!', 'Feedback Submission Failed!');
+      }
+
+      return;
+    }
+    
   }
 }
