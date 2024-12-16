844923d7e2e0e12acece7a0a057bfb2cf773a3c8
ishandwivedi1210@gmail.com <ishandwivedi1210@gmail.com>
2024-12-17 02:04:02 +0530

    minor changes

--- art-corner/src/app/feedback.ts
+++
@@ -1,8 +0,0 @@
-export const sample_feedback: any = [
-    {
-        name: "ABC",
-        email: "abc@gmail.com",
-        subject: "Form Feedback",
-        description: "Very clean website."
-    }
-]
