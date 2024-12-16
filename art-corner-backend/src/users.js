bda902c7ab65282fcbd273d87a45eb432cac4efb
ishandwivedi1210@gmail.com <ishandwivedi1210@gmail.com>
2024-12-16 22:07:50 +0530

    Updates

---
+++ art-corner-backend/src/users.js
@@ -0,0 +1,89 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sample_users = void 0;
+exports.sample_users = [
+    {
+        "_id": "673850b0067a6f93f20d8635",
+        "name": "Gaurav Singh",
+        "email": "gauravsingh@gmail.com",
+        "password": "$2a$10$ncE4mRl61mo9kvo0XHBRK.kgRI0I.EX/iOia6bzPlszGJRkKY938i",
+        "img": "./profiles/profile4.png",
+        "isAdmin": false,
+        "createdAt": "2024-11-16T07:58:40.403Z",
+        "updatedAt": "2024-11-16T07:58:40.403Z",
+        "__v": 0,
+        "id": "673850b0067a6f93f20d8635"
+    },
+    {
+        "_id": "673850dd067a6f93f20d863d",
+        "name": "Gaurav Yadav",
+        "email": "gauravyadav@gmail.com",
+        "password": "$2a$10$bYvEIfyTZRoX31SGMJu.cOEsUrMEoXJmse5QLG5zYF5QIMHd3pIa.",
+        "img": "./profiles/profile1.png",
+        "isAdmin": false,
+        "createdAt": "2024-11-16T07:59:25.632Z",
+        "updatedAt": "2024-11-16T07:59:25.632Z",
+        "__v": 0,
+        "id": "673850dd067a6f93f20d863d"
+    },
+    {
+        "_id": "67385106067a6f93f20d8643",
+        "name": "Kartik Kushwaha",
+        "email": "kartikkushwaha@gmail.com",
+        "password": "$2a$10$1eogyG5R2s17XLRnoQMDMObi4Z9SILLrMHRXLG78zvzIsxyB3F.Aq",
+        "img": "./profiles/profile19.png",
+        "isAdmin": false,
+        "createdAt": "2024-11-16T08:00:06.607Z",
+        "updatedAt": "2024-11-16T08:00:06.607Z",
+        "__v": 0,
+        "id": "67385106067a6f93f20d8643"
+    },
+    {
+        "_id": "67385138067a6f93f20d8649",
+        "name": "Kaushiki Jaiswal",
+        "email": "kaushikijaiswal@gmail.com",
+        "password": "$2a$10$mQVMXFOanJ6k9haOf3mJEeVCzPeuFA1Whp.jL0YNesFDI8OIsfbrK",
+        "img": "./profiles/profile20.png",
+        "isAdmin": false,
+        "createdAt": "2024-11-16T08:00:56.232Z",
+        "updatedAt": "2024-11-16T08:00:56.232Z",
+        "__v": 0,
+        "id": "67385138067a6f93f20d8649"
+    },
+    {
+        "_id": "67385177067a6f93f20d8653",
+        "name": "Ishan Dwivedi",
+        "email": "ishandwivedi@gmail.com",
+        "password": "$2a$10$qk8kbiIj85nI1APrkDxzge7i58loHpMwc2uP8PFKItKycmF7XARwi",
+        "img": "./profiles/profile22.png",
+        "isAdmin": false,
+        "createdAt": "2024-11-16T08:01:59.666Z",
+        "updatedAt": "2024-11-16T08:01:59.666Z",
+        "__v": 0,
+        "id": "67385177067a6f93f20d8653"
+    },
+    {
+        "_id": "673ae6ef03ba261cdd7171a8",
+        "name": "Hridayesh Ojha",
+        "email": "hridayeshojha@gmail.com",
+        "password": "$2a$10$4DtvL.lOl46xCtsbJwV4lOthalYkLex6r.zRFSMg2kjoMSOdv/LrK",
+        "img": "./profiles/profile26.png",
+        "isAdmin": false,
+        "createdAt": "2024-11-18T07:04:15.230Z",
+        "updatedAt": "2024-11-18T07:04:15.230Z",
+        "__v": 0,
+        "id": "673ae6ef03ba261cdd7171a8"
+    },
+    {
+        "_id": "674ae3212da3e0c81c5122e0",
+        "name": "Admin",
+        "email": "admin@artcorner.com",
+        "password": "$2a$10$/DIkAZIHZuHDUJJtkIqIKOCpqrBiXenQhz5/V2xzskURzeEktXNhy",
+        "img": "./profiles/profile29.png",
+        "isAdmin": true,
+        "createdAt": "2024-11-30T10:04:17.302Z",
+        "updatedAt": "2024-11-30T10:04:17.302Z",
+        "__v": 0,
+        "id": "674ae3212da3e0c81c5122e0"
+    }
+];
