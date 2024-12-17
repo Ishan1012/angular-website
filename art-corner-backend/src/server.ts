69ed65b14093b15dbd67159790720cfd73ef12d8
ishandwivedi1210@gmail.com <ishandwivedi1210@gmail.com>
2024-12-17 09:02:37 +0530

    minor changes

--- art-corner-backend/src/server.ts
+++ art-corner-backend/src/server.ts
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:4200"]
+    origin: ["https://art-corner.vercel.app"]
 }));
 
 app.use("/api/artifacts", artifactsRouter);
